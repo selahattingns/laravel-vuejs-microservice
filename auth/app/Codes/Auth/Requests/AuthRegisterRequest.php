5e8ca65ce0e6bcfa3a26c3f0d68f97dac79d0db1
Selahattin GÜNEŞ <36725371+selahattingunes@users.noreply.github.com>
2023-08-06 15:47:45 +0300

    "failedValidation" function updated for requests

--- auth/app/Codes/Auth/Requests/AuthRegisterRequest.php
+++ auth/app/Codes/Auth/Requests/AuthRegisterRequest.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Requests;
 
+use App\Helpers\RedirectHelper;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\JsonResponse;
 
 class AuthRegisterRequest extends FormRequest
 {
@@ -26,12 +25,5 @@ class AuthRegisterRequest extends FormRequest
     /**
      * @param Validator $validator
      */
-    public function failedValidation(Validator $validator)
-    {
-        throw new HttpResponseException(
-            response()->json([
-                'errors' => $validator->errors(),
-            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
-        );
-    }
+    public function failedValidation(Validator $validator) {RedirectHelper::failedValidation($validator); }
 }
